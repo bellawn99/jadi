91716148ac9b96d989ff5ed5013da00cbaf437c3
bellawn99 <bellawulan99@mail.ugm.ac.id>
2020-03-29 12:21:21 +0700

    hapus komentar

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -14,19 +14,14 @@ class LandingController extends Controller
     public function index()
     {
 
-        //ini contoh
         $user = Auth::user(); 
         if(! $user){
             return view('welcome');
         }elseif($user->role_id == 1){
             return redirect()->route('admin.dashboard');
         }else{
-            return redirect()->route('userhome'); // ini di ganti lempar ke landing mu, if dia ga punya role lempar ke landinga
+            return redirect()->route('userhome'); 
             
         }
-
-        //itu kamu lempar ke middleware mu
-
-        return "ini landing page";
     }
 }
